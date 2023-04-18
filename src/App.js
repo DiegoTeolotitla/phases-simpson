37d87d1194ac6525b3e2ab320090b0e83718f502
Diego.Teolotitla <diego.teolotitla@outlook.com>
2023-04-18 00:16:46 -0600

    se agrega estilos de nuevos compoenentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
-import Container from './components/Container';
+import { useState } from "react";
+import "./App.css";
+import Container from "./components/Container";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import CharacterContainer from "./components/CharacterContainer";
 
 function App() {
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async () => {
+    const api = await fetch(
+      "https://thesimpsonsquoteapi.glitch.me/quotes?count=3"
+    );
+    const frase = await api.json();
+    setCharacters(frase);
+  };
+
   return (
     <Container>
-      <p>Recibiendo Children de app</p>
+      <Header />
+      {!characters ? (
+        <Welcome reqApi={reqApi}/>
+      ):(
+        <CharacterContainer characters = {characters} reqApi={reqApi}/>
+      )}
+      
+      
     </Container>
   );
 }
